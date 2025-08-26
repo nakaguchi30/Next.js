101858231d5c96e989a48669ae1ca333b7903f5d
nakaguch30 <nakaguchi@fsi.co.jp>
2025-08-26 14:31:11 +0900

    2nd

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: '700'  // 数値を文字列に変更
+});
